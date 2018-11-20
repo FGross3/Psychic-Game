966816bb3227bbad0d5caf87fb51b1d66141a74c
FGross <44384879+FGross3@users.noreply.github.com>
2018-11-19 17:57:58 -0700

    removed unused file

--- assets/javascript/test.js
+++
@@ -1,3 +0,0 @@
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
-console.log(computerGuess);
